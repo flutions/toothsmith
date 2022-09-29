c6f640a02471f34711fdc89eb9f8f2eecc7df9ec
Flutions <admin@flutions.in>
2022-09-29 19:35:12 +0530

    responsive fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,7 @@ export class HeaderComponent implements OnInit {
   currentTab = 'home';
   viewTab = false;
   sticky = false;
+  checked = false;
   header: any = document.getElementById('header');
   constructor() {}
 
@@ -16,6 +17,15 @@ export class HeaderComponent implements OnInit {
   ngDoCheck(): void {
     this.currentTab = window.location.pathname.split('/')[1];
     // this.sticky = window.pageYOffset > this.header.offsetTop;
+  }
+  nav(data: any) {
+    const height: any = document.getElementById(data)?.offsetHeight;
+    console.log(height);
+
+    document.getElementById('menu-btn')?.click();
+    this.checked = false;
+    document.getElementById(data)?.scrollIntoView();
+    // window.scrollTo(0, height + 100);
   }
 
   // Open Close Navbar Menu on Click Burger
